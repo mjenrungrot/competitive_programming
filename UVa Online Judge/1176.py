00e21ad93ec7d9e9c8005a28f8a84ecc006b7029
Teerapat Jenrungrot <15001582+mjenrungrot@users.noreply.github.com>
2021-03-29 10:57:04 -0700

    add 1176 uva

---
+++ UVa Online Judge/1176.py
@@ -0,0 +1,23 @@
+MAXN = 32770
+dp = [0 for i in range(MAXN)]
+dp[1] = 0
+for i in range(2, MAXN):
+    dp[i] = (dp[i-1] + 2) % i
+
+while True:
+    try:
+        N = int(input())
+    except:
+        break
+    
+    sum_payment = 0
+    ans = 0
+    while True:
+        ans = dp[N] + 1
+        if ans == N:
+            sum_payment += 2*N
+            break
+        sum_payment += N-ans
+        N = ans
+
+    print(sum_payment)
