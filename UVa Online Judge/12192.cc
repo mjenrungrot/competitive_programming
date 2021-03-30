c2f943fadcd521fb6132a3d1583dac97d4b9630b
Teerapat Jenrungrot <15001582+mjenrungrot@users.noreply.github.com>
2021-03-30 10:30:57 -0700

    add 12192 uva

---
+++ UVa Online Judge/12192.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+vector< vector<int> > A;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    while(cin >> N >> M){
+        if(N == 0 and M == 0) break;
+
+        A.clear();
+        for(int i=0;i<N;i++){
+            vector <int> tmp_vec;
+            for(int j=0;j<M;j++){
+                int tmp_val;
+                cin >> tmp_val;
+                tmp_vec.push_back(tmp_val);
+            }
+            A.push_back(tmp_vec);
+        }
+
+        int Q;
+        cin >> Q;
+        for(int i=0;i<Q;i++){
+            int L, R;
+            cin >> L >> R;
+            int ans = 0;
+            for(int r=0;r<N;r++){
+                int left_c  = lower_bound(A[r].begin(), A[r].end(), L) - A[r].begin();
+
+                for(int size=ans+1; size<=N; size++){
+                    if(left_c + size - 1 >= M) break;
+                    if(r + size - 1 >= N) break;
+                    if(A[r+size-1][left_c+size-1] > R) break;
+                    ans = size;
+                }
+            }
+            cout << ans << endl;
+        }
+        cout << "-" << endl;
+    }
+
+    return 0;
+}
