005c1296a192206a03329d60a222fa429a539e46
Teerapat Jenrungrot <15001582+mjenrungrot@users.noreply.github.com>
2021-04-02 10:46:03 -0700

    add 1193 410 uva

---
+++ UVa Online Judge/1193.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, n_case = 0;
+    double D;
+    while(cin >> N >> D){
+        if(N == 0 and D == 0) break;
+
+        vector< tuple<double,double> > A;
+        bool possible = true;
+        for(int i=1;i<=N;i++){
+            int tmp_x, tmp_y;
+            cin >> tmp_x >> tmp_y;
+
+            if(tmp_y > D) possible = false;
+            if(possible){
+                double left = tmp_x - sqrt(D*D - tmp_y*tmp_y);
+                double right = tmp_x + sqrt(D*D - tmp_y*tmp_y);
+                A.push_back(tuple<double,double>(left, right));
+            }
+        }
+        sort(A.begin(), A.end(), [](tuple<double,double> x, tuple<double,double> y){
+            if(get<0>(x) != get<0>(y)) return get<0>(x) < get<0>(y);
+            return get<1>(x) > get<1>(y);
+        });
+
+        int ans = 0;
+        for(int i=0;i<(int)A.size() and possible;i++){
+            double left = get<0>(A[i]);
+            double right = get<1>(A[i]);
+
+            int k = i+1;
+            double best_right = right;
+            while(k < (int)A.size() and get<0>(A[k]) <= best_right){
+                best_right = min(best_right, get<1>(A[k]));
+                k++;
+            }
+            i = k-1;
+            ans++;
+        }
+        if(possible) cout << "Case " << ++n_case << ": " << ans << endl;
+        else cout << "Case " << ++n_case << ": -1" << endl;
+    }
+
+    return 0;
+}
---
+++ UVa Online Judge/410.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, K;
+    int n_test = 0;
+    while(cin >> K >> N){
+        cout << "Set #" << (++n_test) << endl;
+        vector <int> vi;
+
+        double avg = 0.0;
+        for(int i=1;i<=N;i++){
+            int tmp;
+            cin >> tmp;
+            vi.push_back(tmp);
+            avg += (double)tmp;
+        }
+        avg /= (double)K;
+        for(int i=N+1;i<=2*K;i++) vi.push_back(0);
+
+        sort(vi.begin(), vi.end());
+        
+        double ans = 0.0;
+        for(int i=0,j=2*K-1;i<j;i++,j--){
+            ans += fabs((double)(vi[i]+vi[j]) - avg);
+
+            cout << " " << i << ":";
+            if(vi[i] > 0) cout << " " << vi[i];
+            if(vi[j] > 0) cout << " " << vi[j];
+            cout << endl;
+        }
+        cout << "IMBALANCE = " << fixed << setprecision(5) << ans << endl;
+        cout << endl;
+    }
+    return 0;
+}
