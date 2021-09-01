36cfdcedf9ba8208704cbc191899c8caae16d7c2
Teerapat Jenrungrot <15001582+mjenrungrot@users.noreply.github.com>
2021-09-01 15:12:16 -0700

    update port

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -97,4 +97,4 @@ fi
 
 echo "[$PREFIX] Starting code-server..."
 # Now we can run code-server with the default entrypoint
-/usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
+/usr/bin/entrypoint.sh --bind-addr 0.0.0.0:$PORT $START_DIR
