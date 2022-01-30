074b794b649a926e21fd4e87be3161e98414052d
Teerapat Jenrungrot <15001582+mjenrungrot@users.noreply.github.com>
2022-01-30 00:47:13 -0800

    add uva 13215

---
+++ UVa Online Judge/v132/13215.cc
@@ -0,0 +1,148 @@
+/*=============================================================================
+#  Author:          Teerapat Jenrungrot - https://github.com/mjenrungrot/
+#  FileName:        13215.cc
+#  Description:     UVa Online Judge - 13215
+=============================================================================*/
+#include <bits/stdc++.h>
+#pragma GCC optimizer("Ofast")
+#pragma GCC target("avx2")
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef pair<long long, long long> ll;
+typedef pair<double, double> dd;
+typedef tuple<int, int, int> iii;
+typedef tuple<long long, long long, long long> lll;
+typedef tuple<double, double, double> ddd;
+typedef vector<string> vs;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vl;
+typedef vector<vector<long long>> vvl;
+typedef vector<double> vd;
+typedef vector<vector<double>> vvd;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<dd> vdd;
+
+// Debug Snippets
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char* x) { cerr << '\"' << x << '\"'; }
+void __print(const string& x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V>& x) {
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T& x) {
+    int f = 0;
+    cerr << '{';
+    for (auto& i : x) cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v) {
+    __print(t);
+    if (sizeof...(v)) cerr << ", ";
+    _print(v...);
+}
+
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+
+template <class Ch, class Tr, class Container>
+basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr>& os,
+                                  Container const& x) {
+    os << "{ ";
+    for (auto& y : x) os << y << " ";
+    return os << "}";
+}
+
+template <class X, class Y>
+ostream& operator<<(ostream& os, pair<X, Y> const& p) {
+    return os << "[ " << p.first << ", " << p.second << "]";
+}
+
+// End Debug Snippets
+
+vs split(string line) {
+    vs output;
+    istringstream iss(line);
+    string tmp;
+    while (iss >> tmp) {
+        output.push_back(tmp);
+    }
+    return output;
+}
+
+vs split(string line, regex re) {
+    vs output;
+    sregex_token_iterator it(line.begin(), line.end(), re, -1), it_end;
+    while (it != it_end) {
+        output.push_back(it->str());
+        it++;
+    }
+    return output;
+}
+
+const int INF_INT = 1e9 + 7;
+const long long INF_LL = 1e18;
+
+void solve() {
+    int N;
+    cin >> N;
+
+    double W = 0.0, H = 0.0, housing_area = 0.0;
+    int state = 0;
+    while (N--) {
+        char ch;
+        double val;
+        cin >> ch >> val;
+
+        if (ch == 'C') {
+            if (state == 1)
+                W += val;
+            else if (state == 2)
+                H += val;
+            state++;
+        }
+        if (state == 1) W += val;
+        if (state == 2) H += val;
+
+        if (ch == 'C' or ch == 'S')
+            housing_area += val * val;
+        else if (ch == 'T')
+            housing_area += sqrt(3.0) / 4.0 * val * val;
+    }
+    double ans = W * H - housing_area;
+    cout << fixed << setprecision(4) << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+
+    return 0;
+}
